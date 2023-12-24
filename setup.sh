4e20d4b342de4ac6fb9d07b4b22c345df2a0d9a1
BogiceRider <anhnguyen.professional@gmail.com>
2023-12-24 13:39:25 +0700

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,41 +1,33 @@
 #!/bin/bash
+GITPAT=$1
+RESOURCE_GROUP=bogice1-rg
 apiappname=bogice-apim
+aspName=bogice-asp
 apikeyvault=bogice-apim-kv
 apiSecretName=Git-PAT
-RESOURCE_GROUP=bogice1-rg
 upn=$(az ad signed-in-user show --query userPrincipalName -o tsv)
 gitUrl=https://github.com/BogiceRider/APIM.git
 
-# Create App Service plan
-PLAN_NAME=bogice-asp
 
-printf "\nCreating Resource group ... (1/7)\n\n"
-
+printf "\nCreating Resource group ... (1/5)\n\n"
 az group create --name $RESOURCE_GROUP --location centralus
 
-printf "\nCreating App Service plan in Basic tier ... (2/7)\n\n"
+printf "\nCreating App Service plan in Basic tier ... (2/5)\n\n"
+az appservice plan create --name $aspName --resource-group $RESOURCE_GROUP --sku basic --location centralus
 
-az appservice plan create --name $apiappname --resource-group $RESOURCE_GROUP --sku basic --location centralus
+printf "\nCreating API App ... (3/5)\n\n"
+az webapp create --name $apiappname --resource-group $RESOURCE_GROUP --plan $aspName
 
-printf "\nCreating API App ... (3/7)\n\n"
-
-az webapp create --name $apiappname --resource-group $RESOURCE_GROUP --plan $apiappname
-
-printf "\Create keyvault for Git PAT - 1/3 ... (4/7)\n\n"
-
+printf "\Create keyvault for Git PAT - 1/3 ... (4/5)\n\n"
 az keyvault create -n $apikeyvault -g $RESOURCE_GROUP -l centralus
 
-printf "\Set secret- 2/3 ... (4/7)\n\n"
+printf "\Set secret- 2/3 ... (4/5)\n\n"
+az keyvault secret set --vault-name $apikeyvault --name $apiSecretName --value $GITPAT
 
-az keyvault secret set --vault-name $apikeyvault --name $apiSecretName --value "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"
-
-printf "\Set policy - 3/3 ... (4/7)\n\n"
+printf "\Set policy - 3/3 ... (4/5)\n\n"
 az keyvault set-policy -n $apikeyvault --secret-permissions get --upn $upn
 
-GITPAT=$(az keyvault secret show --vault-name $apikeyvault --name $apiSecretName --query value -o tsv)
-
-printf "\nSetting the account-level deployment credentials ...(4/7)\n\n"
-
+printf "\nSetting the account-level deployment credentials ...(5/5)\n\n"
 az webapp deployment source config --name $apiappname --resource-group $RESOURCE_GROUP --repo-url $gitUrl --branch master --git-token $GITPAT --mamual-integration
 
 # Create Web App with GitHub deploy
